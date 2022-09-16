f4c36a0e966617960622e9898d6475bfa71d63a5
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-09-16 20:55:52 +0300

    Add export const BlogCard = ({ poster, tag, title, description, userName, avatar, postedAt }) => {

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,35 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { Card, CardHeader, CardBody, Tag, CardTitle, CardText, CardPoster, CardFooter, UserBox, Avatar, UserInfo, UserName, Date } from './BlogCard.styled';
+// import { format } from 'date-fns'
+// import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+export const BlogCard = ({ poster, tag, title, description, userName, avatar, postedAt }) => {
+  
+  // console.log(formatDistanceToNow(new Date()));
+  
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster
+          src={poster}
+          alt="card__image"
+        />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>
+          {description}
+        </CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={userName} />
+          <UserInfo>
+            <UserName>{userName}</UserName>
+            <Date>{postedAt}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
